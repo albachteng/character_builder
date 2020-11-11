fc84975ba9ad3ccde2fa7507853d4c7eeb65310a
Graham Albachten <albachteng@gmail.com>
2020-11-11 11:28:18 -0600

    finish adding basic weapons and create armory object for export

--- items.js
+++ items.js
@@ -23,7 +23,7 @@
          attack() {
             let attack = Math.floor(Math.random() * this.damage[0]) + 1;
             console.log(`attack = ${attack}`);
-            if this.properties.includes('two-handed') {
+            if (this.properties.includes('two-handed')) {
                 return this.twoHandAttack();
             } else {
                 return attack;
@@ -67,12 +67,12 @@ const club = new Weapon(
 
 const dagger = new Weapon(
     'Dagger',
-    'A small, sharp dagger, which deals piercing damage.',
+    'A small blade, which deals piercing damage.',
     1,
     2,
     ['STR', 'DEX'],
     [4],
-    ['finesse', 'light', 'thrown: range 20/60'],
+    ['finesse', 'light', 'thrown: 20/60'],
 );
 
 const greatclub = new Weapon(
@@ -237,17 +237,37 @@ const greataxe = new Weapon(
     ['heavy', 'reach', 'two-handed'],
 );
 
-const greatsword = new Weapon(
+const greatsword = new Weapon( // NTS: test this one
     'Greatsword',
     'A curved blade at the end of a metal haft, which deals slashing damage.',
     6,
     50,
     ['STR'],
     [0, 6],
-    ['heavy', 'reach', 'two-handed', 'multiple dice'],
+    ['heavy', 'two-handed', 'multiple dice'],
 );
 
- const longsword = new Weapon(
+const halberd = new Weapon(
+    'Halberd',
+    'A axe blade at the end of a metal haft, which deals slashing damage.',
+    6,
+    20,
+    ['STR'],
+    [0, 10],
+    ['heavy', 'reach', 'two-handed'],
+);
+
+const lance = new Weapon(
+    'Lance',
+    'A long metal lance, which deals piercing damage.',
+    6,
+    10,
+    ['STR'],
+    [12, 12],
+    ['reach', 'special: disadvantage when attacking creatures within 5ft of you. Also requires two hands when not mounted'],
+);
+
+const longsword = new Weapon(
     'Longsword', // name
     'A one or two-handed weapon dealing slashing damage', // description
     3, // weight
@@ -255,7 +275,186 @@ const greatsword = new Weapon(
     ['STR'], // STR or DEX
     [8, 10], // attack damage
     ['versatile'],
-    ); // versatile attack damage, if any
+); // versatile attack damage, if any
 
+const maul = new Weapon(
+    'Maul',
+    'A heavy metal hammer, which deals bludgeoning damage.',
+    10,
+    10,
+    ['STR'],
+    [0, 6],
+    ['heavy', 'two-handed', 'multiple dice'],
+);
 
- export default longsword;
+const morningstar = new Weapon(
+    'Morningstar',
+    'A spiked metal mace, which deals piercing damage.',
+    4,
+    15,
+    ['STR'],
+    [8],
+);
+
+const pike = new Weapon(
+    'Pike',
+    'A heavy metal spear, which deals piercing damage.',
+    18,
+    5,
+    ['STR'],
+    [0, 10],
+    ['heavy', 'reach', 'two-handed'],
+);
+
+const rapier = new Weapon(
+    'Rapier',
+    'A thin, flexible blade, which deals piercing damage.',
+    2,
+    25,
+    ['STR', 'DEX'],
+    [8],
+    ['finesse'],
+);
+
+const scimitar = new Weapon(
+    'Scimitar',
+    'A curved blade, which deals slashing damage.',
+    3,
+    25,
+    ['STR', 'DEX'],
+    [6],
+    ['finesse', 'light'],
+);
+
+const shortsword = new Weapon(
+    'Shortsword',
+    'A straight blade, which deals slashing damage.',
+    2,
+    10,
+    ['STR', 'DEX'],
+    [6],
+    ['finesse', 'light'],
+);
+
+const trident = new Weapon(
+    'Trident',
+    'A three-pronged spear, which deals piercing damage.',
+    4,
+    5,
+    ['STR', 'DEX'],
+    [6, 8],
+    ['thrown: 20/60', 'versatile'],
+);
+
+const warpick = new Weapon(
+    'War pick',
+    'A sharp blade on a wooden haft, which deals piercing damage.',
+    2,
+    5,
+    ['STR'],
+    [8],
+);
+
+const warhammer = new Weapon(
+    'Warhammer',
+    'A metal hammer with a narrow head, which deals bludgeoning damage.',
+    2,
+    15,
+    ['STR'],
+    [8, 10],
+    ['versatile'],
+);
+
+const whip = new Weapon(
+    'Whip',
+    'A braided leather whip, which deals slashing damage.',
+    3,
+    2,
+    ['STR', 'DEX'],
+    [4],
+    ['finesse', 'reach'],
+);
+
+// martial ranged weapons
+
+const blowgun = new Weapon(
+    'Blowgun',
+    'A wooden or metal tube, which deals piercing damage.',
+    1,
+    10,
+    ['DEX'],
+    [1],
+    ['ammunition', 'range: 25/100', 'loading'],
+);
+
+const handCrossbow = new Weapon(
+    'Crossbow, hand',
+    'A small crossbow, which deals piercing damage.',
+    3,
+    75,
+    ['DEX'],
+    [6],
+    ['ammunition', 'light', 'range: 30/120', 'loading'],
+);
+
+const heavyCrossbow = new Weapon(
+    'Crossbow, heavy',
+    'A heavy crossbow, which deals piercing damage.',
+    18,
+    50,
+    ['DEX'],
+    [0, 10],
+    ['ammunition', 'heavy', 'range: 100/400', 'loading', 'two-handed'],
+);
+
+const longbow = new Weapon(
+    'Longbow',
+    'A large wooden bow, which deals piercing damage.',
+    2,
+    50,
+    ['DEX'],
+    [8],
+    ['ammunition', 'heavy', 'range: 150/600', 'two-handed'],
+);
+
+const net = new Weapon(
+    'Net',
+    'A rope net, which deals no damage but can .',
+    3,
+    1,
+    ['DEX'],
+    [0],
+    ['thrown: 5/15', 'special: a large or smaller creature hit with this weapon is restrained until freed with a DC10 STR check.'],
+);
+
+// the armory object, to be exported 
+const armory = {
+    weapons: {
+        club, dagger, greatclub, 
+        handaxe, javelin, lightHammer, 
+        mace, quarterstaff, sickle,
+        spear, lightCrossbow, dart,
+        shortbow, sling, battleaxe,
+        flail, glaive, greataxe,
+        greatsword, halberd, lance,
+        longsword, maul, morningstar,
+        pike, rapier, scimitar,
+        shortsword, trident, warpick,
+        warhammer, whip, blowgun,
+        handCrossbow, heavyCrossbow, longbow,
+        net, 
+    },
+    armor: {
+
+    },
+    items: {
+
+    }, 
+    potions: {
+        
+    },
+}
+
+console.log(armory.weapons.dagger);
+
+ export default armory;
