ac3af32d29b9d006f1d3311e526701a12acdbaa1
Graham Albachten <albachteng@gmail.com>
2020-11-10 20:37:08 -0600

    additional races, item testing and removed level parameter from characters, instead initializing at 1

--- characters.js
+++ characters.js
@@ -1,14 +1,19 @@
+import longsword from './items.js';
+
+// NTS: node -r ems characters.js to allow imports in node.js
 // characters and their class extensions (fighter, etc)
 
 class Character {
-    constructor(name, level, race, subrace) {
+    constructor(name, race, subrace) {
         this.name = name; // string
-        this.level = level; // number; important for class abilities later on
+        this.level = 1; // number; important for class abilities later on
         this.items = []; // array of items, see 'item.js'; initialized empty
         this.abilities = [];
         this.proficiencies = [];
+        this.speed = 0;
         this.languages = ['Common'];
         this.hitDiceNumber = 1; // number of hit dice, type is determined by class 
+        this.hitDiceType = 0;
         this.MAXHP = 0; // starting HP determined by class
         this.HP = 0; // current HP determined by class
         this.AC = 10; // partly determined by items; w/out armor or shield 10+DEX
@@ -26,6 +31,8 @@ class Character {
 // here we start the race section, which I think just has to be a part of the character creation
 // NTS: need to update to include subraces;
 
+        // DWARVES
+
         if (race === 'dwarf') {
             if (subrace === 'hill') {
                 this.abilityScore.WIS += 1;
@@ -38,7 +45,7 @@ class Character {
             this.abilityScore.CON += 2;
             this.languages.push('Dwarven');
             this.size = 'medium';
-            this.speed = 25;
+            this.speed += 25;
             this.abilities.push({
                 name: 'Darkvision',
                 description: 'Accustomed to life underground, you have superior vision in dark and dim conditions. You can see in dim light within 60 feet of you as if it were bright light, and in darkness as if it were dim light. You can’t discern color in darkness, only shades of gray.',
@@ -60,42 +67,136 @@ class Character {
                 'battleaxe', 'handaxe', 'throwing hammer', 'warhammer',
                 );
         }
-        if (race === 'human') {
 
-        }
+        // ELVES
+        
         if (race === 'elf') {
+            if (this.subrace === 'high') {
+                this.abilityScore.INT += 1;
+                this.proficiencies.push(
+                    'longsword', 'shortsword', 'shortbow', 'longbow',
+                );
+                this.abilities.push(
+                    {
+                        name: 'Cantrip',
+                        description: 'You know one cantrip of your choice from the wizard spell list. Intelligence is your spellcasting ability for it.',
+                    },
+                    {
+                        name: 'Extra Language',
+                        description: 'You can speak, read, and write one extra language of your choice.',
+                    },
+                )
+            }
+            if (this.subrace === 'wood') {
+                this.abilityScore.WIS += 1;
+                this.proficiencies.push(
+                    'longsword', 'shortsword', 'shortbow', 'longbow',
+                );
+                this.speed += 5;
+                this.abilities.push(
+                    {
+                        name: 'Mask of the Wild',
+                        description: 'You can attempt to hide even when you are only lightly obscured by foliage, heavy rain, falling snow, mist, and other natural phenomena.',
+                    },
+                );
+            }
+            if (this.subrace === 'drow') {
+                this.abilityScore.CHA += 1;
+                this.abilities.push(
+                    {
+                        name: 'Superior Darkvision',
+                        description: 'Your darkvision has a radius of 120 feet.', 
+                    },
+                    {
+                        name: 'Sunlight Sensitivity', 
+                        description: 'You have disadvantage on attack rolls and on Wisdom (Perception) checks that rely on sight when you, the target of your attack, or whatever you are trying to perceive is in direct sunlight.',
+                    },
+                    {
+                        name: 'Drow Magic',
+                        description: 'You know the dancing lights cantrip. When you reach 3rd level, you can cast the faerie fire spell once per day. When you reach 5th level, you can also cast the darkness spell once per day. Charisma is your spellcasting ability for these spells.',
+                    },
+                );
+                this.proficiencies.push('rapier', 'shortsword', 'hand crossbow',);
+            }
             this.abilityScore.DEX += 2;
-            this.languages.push('elvish');
+            this.languages.push('Elvish');
             this.size = 'medium';
-            this.speed = 30;
+            this.speed += 30;
             this.abilities.push(
                 {
                     name: 'Darkvision',
                     description: 'Accustomed to twilit forests and the night sky, you have superior vision in dark and dim conditions. You can see in dim light within 60 feet of you as if it were bright light, and in darkness as if it were dim light. You can’t discern color in darkness, only shades of gray.',
+                },
+                {
+                    name: 'Fey Ancestry',
+                    description: 'You have advantage on saving throws against being charmed, and magic can’t put you to sleep.',
+                },
+                {
+                    name: 'Trance', 
+                    description: 'Elvesdon’tneedtosleep.Instead,they meditate deeply, remaining semiconscious, for 4 hours a day. While meditating, you can dream after a fashion; such dreams are actually mental exercises that have become reflexive through years of practice. After resting in this way, you gain the same benefit that a human does from 8 hours of sleep.',
+                },
+            );
+            this.proficiencies.push('perception',);
+        }
+        // HUMANS
+        
+        if (race === 'human') {
+            this.abilityScore.STR++;
+            this.abilityScore.DEX++;
+            this.abilityScore.CON++;
+            this.abilityScore.INT++;
+            this.abilityScore.WIS++;
+            this.abilityScore.CHA++;
+            this.size = 'medium';
+            this.speed += 30;
+            this.abilities.push(
+                {
+                    name: 'Bonus Language',
+                    description: 'You can speak, read and write one additional language of your choice.'
                 }
             )
         }
-        if (race === '')
     }
     getItems() {
+        return this.items;
+    }
+    addItem(item) {
+        this.items.push(item); // adds an item object to the items array
         return this.items;
     }
     setAbilityScore(stat, num) { // takes a stat key and a number
         this.abilityScore[stat] = num;
         return this.abilityScore;
     }
-    addItem(item) {
-        this.items.push(item); // adds an item object to the items array
-        return this.items;
+    attack(item, versatile) {
+        let modifier = Math.floor((this.abilityScore.STR - 10) / 2);
+        let attackRoll = Math.floor(Math.random() * 20) + 1;
+        console.log(`modifier = ${modifier}`);
+        console.log(`attack roll = ${attackRoll}`);
+        if (versatile) {
+            return [attackRoll + modifier, item.twoHandAttack() + modifier];
+        } else {
+            return [attackRoll + modifier, item.attack() + modifier];
+        }
+    }
+    levelUp(attribute1, attribute2) {
+        this.level++;
+        this.hitDiceNumber++;
+        this.MAXHP += Math.floor(Math.random() * this.hitDiceType);
+        this.HP = this.MAXHP;
+        if (this.level % 4 === 0) {
+            this.abilityScore[attribute1]++;
+            this.abilityScore[attribute2]++;
+        }
     }
 }
 
 // character classes below
 
 class Fighter extends Character {
-    constructor(name, level, race, subrace) {
-        super(name, level, race, subrace);
-        this.hitDice = 10; // the TYPE of hit dice, not number. See hitDiceNumber property
+    constructor(name, race, subrace) {
+        super(name, race, subrace);
+        this.hitDiceType += 10; // the VALUE of hit dice, not number. See hitDiceNumber property
         this.MAXHP += 10;
         this.HP += 10;
         this.abilities.push(
@@ -113,5 +214,10 @@ class Fighter extends Character {
 
 // test subject
 
-const isho = new Fighter('Isho-Genni', 1, 'dwarf', 'mountain');
-console.log(isho.proficiencies);
+const isho = new Fighter('Isho-Genni', 'elf', 'drow');
+isho.setAbilityScore('STR', 15);
+isho.addItem(longsword);
+isho.levelUp();
+isho.levelUp();
+isho.levelUp('DEX', 'STR');
+console.log(isho);
--- items.js
+++ items.js
@@ -1,19 +1,46 @@
  class Item {
-    constructor(name, description) {
+    constructor(name, description, weight) {
         this.name = name; // string;
         this.description = description; // string;
+        this.weight = weight;
     }
  }
 
  class Potion extends Item {
-     constructor(name, description) {
+     constructor(name, description, weight) {
          super(name, description);
      }
  }
 
  class Weapon extends Item {
-     constructor(name, description, damage) {
-         super(name, description);
+     constructor(name, description, weight, damage, versatile) {
+         super(name, description, weight);
+         this.versatile = versatile;
          this.damage = damage; // might have to represent this with more than one property
+         };
+         attack() {
+            let attack = Math.floor(Math.random() * this.damage) + 1;
+            console.log(`attack = ${attack}`);
+            return attack;
+        }
+        twoHandAttack() {
+            if (this.versatile) {
+                let attack = Math.floor(Math.random() * this.versatile) + 1;
+                console.log(`attack = ${attack}`);
+                return attack;
+            } else {
+                return this.attack();
+            }
+        }
+ }
+
+ class Armor extends Item {
+     constructor(name, description, weight, AC) {
+         super(name, description, weight);
+         this.AC = AC;
      }
  }
+
+const longsword = new Weapon('Longsword', 'A one or two-handed weapon dealing slashing damage', 3, 8, 10);
+
+ export default longsword;
